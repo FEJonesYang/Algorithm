b88d9be5985a07abded325990d043d8dd9f5ae6a
Yangchao <545661033@qq.com>
2021-05-29 18:21:15 +0800

    双指针 ---> 快慢指针，同Solution26类似

---
+++ Tencent/src/Solution27.java
@@ -0,0 +1,25 @@
+/**
+ * @Author: JonesYang
+ * @Data: 2021-05-29
+ * @Description: 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution27 {
+    public int removeElement(int[] nums, int val) {
+        // 定义两个指针
+        int low = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[low++] = nums[fast];
+            }
+            fast++;
+        }
+        return low;
+    }
+}
