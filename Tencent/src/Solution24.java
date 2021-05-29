5aea679b7ee6f4e13d034e5e0ab898acab3a0051
Yangchao <545661033@qq.com>
2021-05-29 16:25:30 +0800

    链表问题，递归和遍历两种不同的解法

--- Tencent/src/Solution24.java
+++ Tencent/src/Solution24.java
@@ -6,16 +6,45 @@
  * . 两两交换链表中的节点
  */
 public class Solution24 {
-    public ListNode swapPairs(ListNode head) {
 
-        ListNode pre = head;
-        ListNode cur = pre.next;
-        while (cur != null) {
-            ListNode temp = pre;
+    /**
+     * 迭代解法，对整个链表进行遍历
+     */
+    public ListNode swapPairs1(ListNode head) {
+        // 定义一个额外的节点
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode cur = pre;
+
+        while (cur.next != null && cur.next.next != null) {
+            // 需要交换的左节点
+            ListNode left = cur.next;
+            // 需要交换的右节点
+            ListNode right = cur.next.next;
+
+            // 开始进行交换，这是这道题的核心
+            cur.next = right;
+            left.next = right.next;
+            right.next = left;
+            // 更新 cur 的节点，进行下一次的交换
+            cur = cur.next.next;
         }
-
-        return head;
+        return pre.next;
     }
+
+    /**
+     * 递归解法
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
 
     public class ListNode {
         int val;
