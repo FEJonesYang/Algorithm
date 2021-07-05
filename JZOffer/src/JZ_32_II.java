965cc29b400e8da32e94929e33d165ae67627dbe
yangchao <545661033@qq.com>
2021-07-05 10:35:39 +0800

    树：层序遍历，需要对树进行分层打印，当前层的节点数等于当前 Queue 的 size

---
+++ JZOffer/src/JZ_32_II.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+/**
+ * @Author: JonesYang
+ * @Data: 2021-07-05
+ * @Description: 剑指 Offer 32 - II. 从上到下打印二叉树 II
+ * <p>
+ * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ * <p>
+ *  
+ * <p>
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class JZ_32_II {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            // 使用 len 记录当前层的节点数
+            int len = deque.size();
+            // 只需要遍历当前的节点数就行了
+            for (int i = 0; i < len; i++) {
+                TreeNode node = deque.remove();
+                temp.add(node.val);
+                if (node.left != null) {
+                    deque.add(node.left);
+                }
+                if (node.right != null) {
+                    deque.add(node.right);
+                }
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
