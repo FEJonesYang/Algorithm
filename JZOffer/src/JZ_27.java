636e19e96cb57d551bfa5b40b56672e7b1d1d420
yangchao <545661033@qq.com>
2021-07-07 10:47:07 +0800

    树：输出一个树的镜像二叉树

---
+++ JZOffer/src/JZ_27.java
@@ -0,0 +1,63 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @Author: JonesYang
+ * @Data: 2021-07-07
+ * @Description: 剑指 Offer 27. 二叉树的镜像
+ * <p>
+ * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+ * <p>
+ * 例如输入：
+ * <p>
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 镜像输出：
+ * <p>
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class JZ_27 {
+    public TreeNode mirrorTree(TreeNode root) {
+        // 递归终止条件
+        if (root == null) return null;
+
+        // 当层数据节点的改变
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+
+        // 递归下一轮
+        mirrorTree(root.right);
+        mirrorTree(root.left);
+        // 返回根节点
+        return root;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
