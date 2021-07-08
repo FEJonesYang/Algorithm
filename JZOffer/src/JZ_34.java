8f0f90748fc77afcc08b137c13a0d12146a888ed
yangchao <545661033@qq.com>
2021-07-08 19:05:27 +0800

    树：二叉树的路径和等于某一个 target，使用先序遍历，回溯算法

---
+++ JZOffer/src/JZ_34.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: JonesYang
+ * @Data: 2021-07-08
+ * @Description: 剑指 Offer 34. 二叉树中和为某一值的路径
+ * <p>
+ * 输入一棵二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径。从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * 给定如下二叉树，以及目标和 target = 22，
+ * <p>
+ * 5
+ * / \
+ * 4   8
+ * /   / \
+ * 11  13  4
+ * /  \    / \
+ * 7    2  5   1
+ * 返回:
+ * <p>
+ * [
+ * [5,4,11,2],
+ * [5,8,4,5]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class JZ_34 {
+
+    LinkedList<List<Integer>> res = new LinkedList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+        recur(root, target);
+        return res;
+    }
+
+    private void recur(TreeNode root, int target) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        target -= root.val;
+
+        // 判断叶子节点的标准---> root.left == null && root.right == null
+        if (target == 0 && root.left == null && root.right == null) {
+            res.add(new LinkedList<>(path));
+        }
+        recur(root.left, target);
+        recur(root.right, target);
+        path.removeLast();
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
