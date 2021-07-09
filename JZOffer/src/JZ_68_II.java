c9aabb5f361abe818af02b4046773fbfcbf81a38
yangchao <545661033@qq.com>
2021-07-09 21:08:06 +0800

    树：树的最近公共祖先，二叉搜索树充分利用性质

---
+++ JZOffer/src/JZ_68_II.java
@@ -0,0 +1,74 @@
+import javax.print.AttributeException;
+
+/**
+ * @Author: JonesYang
+ * @Data: 2021-07-08
+ * @Description: 剑指 Offer 68 - II. 二叉树的最近公共祖先
+ * <p>
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ * <p>
+ * <p>
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ * 示例 2:
+ * <p>
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出: 5
+ * 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class JZ_68_II {
+    /**
+     * 核心思路：前提条件是二叉搜索树，p 和 q 要么位于同一侧，要么位于这个父节点的两侧
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 递归终止条件判断，如果返回 null，就是知道叶子节点也没有找到 q p。
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // p q 一个在左，一个在右
+        if (left != null && right != null) {
+            return root;
+        }
+        // p q 都在左
+        if (left != null) {
+            return left;
+        }
+        // p q 都在右
+        if (right != null) {
+            return right;
+        }
+        return null;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
