9272f95f4569c006a5ca20fd8797cd4a2fb9685f
yangchao <545661033@qq.com>
2021-07-11 15:45:43 +0800

    链表：一个带有 random 的指针的链表，要求完成链表的复制

---
+++ JZOffer/src/JZ_35.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: JonesYang
+ * @Data: 2021-07-11
+ * @Description: 剑指 Offer 35. 复杂链表的复制
+ * <p>
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，
+ * 还有一个 random 指针指向链表中的任意节点或者 null。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class JZ_35 {
+    public Node copyRandomList(Node head) {
+        // base
+        if (head == null) return null;
+
+        Map<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        // 完成 next 和 random 的指向
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+}
