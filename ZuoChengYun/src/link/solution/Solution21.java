2faaae8d2ee6225a94a439c5cced6ee02ebc94b5
yangchao <545661033@qq.com>
2021-07-13 00:16:04 +0800

    链表：合并两个有序的链表

---
+++ ZuoChengYun/src/link/solution/Solution21.java
@@ -0,0 +1,58 @@
+package link.solution;
+
+/**
+ * @Author: JonesYang
+ * @Data: 2021-07-12
+ * @Description:
+ */
+public class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        ListNode newHead = new ListNode(-1);
+        ListNode newCur = newHead;
+
+        // 当这两个链表都没有结束的时候
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val < cur2.val) {
+                newCur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                newCur.next = cur2;
+                cur2 = cur2.next;
+            }
+            newCur = newCur.next;
+        }
+
+        // 如果这两个链表其中一个为null
+        if (cur1 != null) {
+            newCur.next = cur1;
+        }
+        if (cur2 != null) {
+            newCur.next = cur2;
+        }
+
+        return newHead.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
